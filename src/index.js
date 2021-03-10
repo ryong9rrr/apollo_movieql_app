7b100b3ba5eed3d3510ad645dc8e429a97c610de
Yong SangYoon <64957267+ryong9rrr@users.noreply.github.com>
2021-03-10 23:27:42 +0900

    useQuery

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App.js";
+import { ApolloProvider } from "@apollo/client";
+import client from "./routes/apollo.js";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById("root")
 );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,21 @@
 import React from "react";
+import { useQuery, gql } from "@apollo/client";
 
-export default () => <h1>Home</h1>;
+const GET_MOVIES = gql`
+  query {
+    movies {
+      id
+      title
+    }
+  }
+`;
+
+export default () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error</p>;
+
+  if (data && data.movies) {
+    return data.movies.map((m) => <h1>{m.id}</h1>);
+  }
+};
