9144367335ac21f230203bc6f66acb53c46706a1
Yong SangYoon <64957267+ryong9rrr@users.noreply.github.com>
2021-03-11 16:18:52 +0900

    graphQL API query 불러오기

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,3 +1,23 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useQuery, gql } from "@apollo/client";
 
-export default () => "Detail";
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_full
+    }
+  }
+`;
+
+export default () => {
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id: +id },
+  });
+  console.log(loading, data);
+  return "Detail";
+};
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,21 +1,61 @@
 import React from "react";
+import styled from "styled-components";
 import { useQuery, gql } from "@apollo/client";
+import Movie from "../components/Movie.js";
 
 const GET_MOVIES = gql`
   query {
     movies {
       id
-      title
+      medium_cover_image
     }
   }
 `;
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
 export default () => {
   const { loading, error, data } = useQuery(GET_MOVIES);
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error</p>;
-
-  if (data && data.movies) {
-    return data.movies.map((m) => <h1>{m.id}</h1>);
-  }
+  return (
+    <Container>
+      <Header>
+        <Title>Apollo 2020</Title>
+        <Subtitle>graphQL practice</Subtitle>
+      </Header>
+      {loading && <Loading>Loading...</Loading>}
+      {error && <Loading>Error...</Loading>}
+      {!loading &&
+        data.movies &&
+        data.movies.map((m) => <Movie key={m.id} id={m.id} />)}
+    </Container>
+  );
 };
